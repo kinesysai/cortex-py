9b31756383a4db6ecf2a2f08696c14cb41aff9a2
cpun2 <83794340+cpun2@users.noreply.github.com>
2023-07-17 12:13:31 -0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,12 @@
+"""A templated cortex callable!
+
+Cortex docs:
+    https://docs.trycortex.ai
+
+Cortex callable example:
+    https://github.com/kinesysai/cortex-py/template.py
+"""
+
+import trycortex
+
+callable = trycortex.addBlocks()
